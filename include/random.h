5b7a18967c585f63d617d11ddf419fcbde63d1ab
Bo <bhock94@gmail.com>
2019-11-26 13:18:23 -0600

    press F pt 2

--- include/random.h
+++ include/random.h
@@ -12,7 +12,7 @@ u16 Random2(void);
 #define Random32() (Random() | (Random() << 16))
 
 //Returns DBK random number
-s32 RandomDBK(s32 max);
+s32 RandomDBK(s32 n);
 
 //Sets the initial seed value of the pseudorandom number generator
 void SeedRng(u16 seed);
