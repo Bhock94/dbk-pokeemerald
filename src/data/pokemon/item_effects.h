7a8eeb1dcf890e42828728dce8abb9b9df86b582
Bo <bhock94@gmail.com>
2019-11-29 00:39:04 -0600

    kings rock attempt 2

--- src/data/pokemon/item_effects.h
+++ src/data/pokemon/item_effects.h
@@ -308,6 +308,10 @@ const u8 gItemEffect_LeafStone[6] = {
     [4] = ITEM4_EVO_STONE,
 };
 
+const u8 gItemEffect_KingsRock[6] = {
+    [4] = ITEM4_EVO_STONE,
+}
+
 const u8 gItemEffect_CheriBerry[6] = {
     [3] = ITEM3_PARALYSIS,
 };
@@ -456,6 +460,7 @@ const u8 *const gItemEffectTable[] =
     [ITEM_THUNDER_STONE - ITEM_POTION] = gItemEffect_ThunderStone,
     [ITEM_WATER_STONE - ITEM_POTION] = gItemEffect_WaterStone,
     [ITEM_LEAF_STONE - ITEM_POTION] = gItemEffect_LeafStone,
+    [ITEM_KINGS_ROCK - ITEM_POTION] = gItemEffect_KingsRock,
     [ITEM_CHERI_BERRY - ITEM_POTION] = gItemEffect_CheriBerry,
     [ITEM_CHESTO_BERRY - ITEM_POTION] = gItemEffect_ChestoBerry,
     [ITEM_PECHA_BERRY - ITEM_POTION] = gItemEffect_PechaBerry,
