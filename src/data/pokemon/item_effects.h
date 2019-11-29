9c1c62f44aaf83f180ad0e43ba14495fd2cbe735
Bo <bhock94@gmail.com>
2019-11-29 12:57:30 -0600

    another test

--- src/data/pokemon/item_effects.h
+++ src/data/pokemon/item_effects.h
@@ -284,7 +284,7 @@ const u8 gItemEffect_XSpecial[8] = {
     [7] = 1,
 };
 
-/*const u8 gItemEffect_SunStone[6] = {
+const u8 gItemEffect_SunStone[6] = {
     [4] = ITEM4_EVO_STONE,
 };
 
@@ -309,10 +309,6 @@ const u8 gItemEffect_LeafStone[6] = {
 };
 
 const u8 gItemEffect_KingsRock[6] = {
-    [4] = ITEM4_EVO_STONE,
-};*/
-
-const u8 gItemEffect_EvoStone[6] = {
     [4] = ITEM4_EVO_STONE,
 };
 
@@ -458,13 +454,12 @@ const u8 *const gItemEffectTable[] =
     [ITEM_X_SPEED - ITEM_POTION] = gItemEffect_XSpeed,
     [ITEM_X_ACCURACY - ITEM_POTION] = gItemEffect_XAccuracy,
     [ITEM_X_SPECIAL - ITEM_POTION] = gItemEffect_XSpecial,
-    [ITEM_SUN_STONE - ITEM_POTION] = gItemEffect_EvoStone,
-    [ITEM_MOON_STONE - ITEM_POTION] = gItemEffect_EvoStone,
-    [ITEM_FIRE_STONE - ITEM_POTION] = gItemEffect_EvoStone,
-    [ITEM_THUNDER_STONE - ITEM_POTION] = gItemEffect_EvoStone,
-    [ITEM_WATER_STONE - ITEM_POTION] = gItemEffect_EvoStone,
-    [ITEM_LEAF_STONE - ITEM_POTION] = gItemEffect_EvoStone,
-    [ITEM_KINGS_ROCK - ITEM_POTION] = gItemEffect_EvoStone,
+    [ITEM_SUN_STONE - ITEM_POTION] = gItemEffect_SunStone,
+    [ITEM_MOON_STONE - ITEM_POTION] = gItemEffect_MoonStone,
+    [ITEM_FIRE_STONE - ITEM_POTION] = gItemEffect_FireStone,
+    [ITEM_THUNDER_STONE - ITEM_POTION] = gItemEffect_ThunderStone,
+    [ITEM_WATER_STONE - ITEM_POTION] = gItemEffect_WaterStone,
+    [ITEM_LEAF_STONE - ITEM_POTION] = gItemEffect_LeafStone,
     [ITEM_CHERI_BERRY - ITEM_POTION] = gItemEffect_CheriBerry,
     [ITEM_CHESTO_BERRY - ITEM_POTION] = gItemEffect_ChestoBerry,
     [ITEM_PECHA_BERRY - ITEM_POTION] = gItemEffect_PechaBerry,
@@ -482,4 +477,5 @@ const u8 *const gItemEffectTable[] =
     [ITEM_GREPA_BERRY - ITEM_POTION] = gItemEffect_GrepaBerry,
     [ITEM_TAMATO_BERRY - ITEM_POTION] = gItemEffect_TamatoBerry,
     [LAST_BERRY_INDEX - ITEM_POTION] = NULL
+    [ITEM_KINGS_ROCK - ITEM_POTION] = gItemEffect_KingsRock,
 };
