829114316433b5bb082b033cfbc31a11b138e189
Bo <bhock94@gmail.com>
2019-11-28 22:54:40 -0600

    test 100

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -282,7 +282,7 @@ static u8 ChooseWildMonLevel(const struct WildPokemon *wildPokemon)
             avg = levelSum / numInParty;
     }
 
-    avg = 50;
+    avg = 100;
 
     // new probability formula
     prob = RandomDBK(100);
